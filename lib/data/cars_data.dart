e451fbc382c299581b0e756652310121f7929685
abellilo <abel.ayinde@gmail.com>
2023-07-26 17:28:47 +0100

    Food App

---
+++ lib/data/cars_data.dart
@@ -0,0 +1,52 @@
+List<Map> listOfData = [
+  {
+    'name': 'Fusion Feast',
+    'shortDescription': '',
+    'year': '20 mins',
+    'hp': '5.0',
+    'speed': '15%',
+    'price': '230',
+    'image':
+    'https://adeoropelumi.com/food/baked-chicken-wings-asian-style-tomatoes-sauce-plate-removebg-preview.png'
+  },
+  {
+    'name': 'Hangout Deal',
+    'shortDescription': '',
+    'year': '15 mins',
+    'hp': '4.5',
+    'speed': '2%',
+    'price': '100',
+    'image':
+    'https://adeoropelumi.com/food/delicious-fried-chicken-plate-removebg-preview.png'
+  },
+  {
+    'name': 'Big King Burger',
+    'shortDescription': '',
+    'year': '12 mins',
+    'hp': '4.8',
+    'speed': '2%',
+    'price': '75',
+    'image':
+    'https://adeoropelumi.com/food/double-hamburger-isolated-white-background-fresh-burger-fast-food-with-beef-cream-cheese-removebg-preview.png'
+  },
+  {
+    'name': 'Maxi Pasta',
+    'shortDescription': '',
+    'year': '17 mins',
+    'hp': '4.9',
+    'speed': '5%',
+    'price': '105',
+    'image':
+    'https://adeoropelumi.com/food/pasta-fettuccine-bolognese-with-tomato-sauce-white-bowl-removebg-preview.png'
+  },
+  {
+    'name': 'Lettuce Combo Salad',
+    'shortDescription': '',
+    'year': '5 mins',
+    'hp': '4.7',
+    'speed': '0%',
+    'price': '30',
+    'image':
+    'https://adeoropelumi.com/food/Screen_Shot_2023-07-26_at_11.14.12_AM-removebg-preview.png'
+  },
+];
